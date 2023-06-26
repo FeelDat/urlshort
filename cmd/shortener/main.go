64bc77528e384e0aed07515401e03aca3a1a912d
feeldat <alimaldybergenof@gmail.com>
2023-06-26 07:04:55 +0600

    added zap logger support

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,22 +4,30 @@ import (
 	"github.com/FeelDat/urlshort/internal/app/config"
 	"github.com/FeelDat/urlshort/internal/app/handlers"
 	"github.com/FeelDat/urlshort/internal/app/storage"
+	logger2 "github.com/FeelDat/urlshort/internal/logger"
+	"github.com/FeelDat/urlshort/internal/middleware"
 	"github.com/go-chi/chi/v5"
-	"log"
 	"net/http"
 )
 
 func main() {
 
-	conf, err := config.NewConfig()
+	logger, err := logger2.InitLogger("Info")
 	if err != nil {
-		log.Fatalf("Failed to initialize config: %v", err)
+		panic(err)
 	}
 
+	conf, err := config.NewConfig()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	loggerMiddleware := middleware.NewLoggerMiddleware(logger)
 	r := chi.NewRouter()
 	mapStorage := storage.NewInMemoryStorage()
 	h := handlers.NewHandler(mapStorage, conf.BaseAddress)
 
+	r.Use(loggerMiddleware.LoggerMiddleware)
 	r.Route("/", func(r chi.Router) {
 		r.Post("/", h.ShortenURL)
 		r.Get("/{id}", h.GetFullURL)
@@ -27,6 +35,7 @@ func main() {
 
 	err = http.ListenAndServe(conf.ServerAddress, r)
 	if err != nil {
-		panic(err)
+		logger.Fatal(err)
 	}
+
 }
---
+++ internal/middleware/logging-middleware.go
@@ -0,0 +1,67 @@
+package middleware
+
+import (
+	"go.uber.org/zap"
+	"net/http"
+	"time"
+)
+
+type responseData struct {
+	status int
+	size   int
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	responseData *responseData
+}
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
+type Middleware struct {
+	logger *zap.SugaredLogger
+}
+
+func NewLoggerMiddleware(logger *zap.SugaredLogger) *Middleware {
+	return &Middleware{
+		logger: logger,
+	}
+}
+
+func (m *Middleware) LoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+
+		next.ServeHTTP(&lw, r)
+
+		duration := time.Since(start)
+
+		m.logger.Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", responseData.status,
+			"duration", duration,
+			"size", responseData.size,
+		)
+	})
+}
