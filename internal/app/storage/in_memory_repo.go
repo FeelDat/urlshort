a08aa854a657613cc48eb8d582d9cd1da9d4dc49
feeldat <alimaldybergenof@gmail.com>
2023-07-31 02:02:11 +0600

    test commit iter14

--- internal/app/storage/in_memory_repo.go
+++ internal/app/storage/in_memory_repo.go
@@ -2,98 +2,120 @@ package storage
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"github.com/FeelDat/urlshort/internal/app/models"
 	"github.com/FeelDat/urlshort/internal/utils"
 	"math/rand"
+	"os"
 )
 
-type MemoryStorage struct {
-	urlMap  map[string]*models.URL
-	userMap map[string][]*models.URL
+type URLInfo struct {
+	UUID        string `json:"uuid"`
+	ShortURL    string `json:"short_url"`
+	OriginalURL string `json:"original_url"`
 }
 
-func NewMemoryStorage() Repository {
-	return &MemoryStorage{
-		urlMap:  make(map[string]*models.URL),
-		userMap: make(map[string][]*models.URL),
-	}
+type storage struct {
+	Links    map[string]string
+	UserURLs map[string][]models.UsersURLS
+	file     *os.File
 }
 
-func (m *MemoryStorage) GetUsersURLS(ctx context.Context, userID string) ([]models.UsersURLS, error) {
-	userUrls, exists := m.userMap[userID]
-	if !exists {
-		return nil, errors.New("no urls found for the user")
+func NewInMemStorage(filePath string) (Repository, error) {
+	var file *os.File
+	var err error
+
+	if filePath != "" {
+		file, err = os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	var urls []models.UsersURLS
-
-	for _, url := range userUrls {
-		urls = append(urls, models.UsersURLS{
-			ShortURL:    url.ShortURL,
-			OriginalURL: url.OriginalURL,
-		})
-	}
-
-	return urls, nil
+	return &storage{
+		Links:    make(map[string]string),
+		UserURLs: make(map[string][]models.UsersURLS),
+		file:     file,
+	}, err
 }
 
-func (m *MemoryStorage) ShortenURL(ctx context.Context, fullLink string) (string, error) {
-	shortURL := utils.Base62Encode(rand.Uint64())
-	userID := ctx.Value("userID").(string)
-
-	if _, exists := m.urlMap[shortURL]; exists {
-		return "", errors.New("short url already exists")
+func (s *storage) GetUsersURLS(ctx context.Context, userID string) ([]models.UsersURLS, error) {
+	if urls, ok := s.UserURLs[userID]; ok {
+		return urls, nil
 	}
 
-	url := &models.URL{
-		ShortURL:    shortURL,
+	return nil, errors.New("no URLs found for the given userID")
+}
+
+func (s *storage) ShortenURL(ctx context.Context, fullLink string) (string, error) {
+	urlID := utils.Base62Encode(rand.Uint64())
+	uid := ctx.Value("userID")
+
+	urlInfo := URLInfo{
+		UUID:        uid.(string),
+		ShortURL:    urlID,
 		OriginalURL: fullLink,
-		UserID:      userID,
 	}
 
-	m.urlMap[shortURL] = url
-	m.userMap[userID] = append(m.userMap[userID], url)
-
-	return shortURL, nil
-}
-
-func (m *MemoryStorage) GetFullURL(ctx context.Context, shortLink string) (string, error) {
-	url, exists := m.urlMap[shortLink]
-	if !exists {
-		return "", errors.New("short url does not exist")
+	data, err := json.Marshal(&urlInfo)
+	if err != nil {
+		return "", err
 	}
 
-	return url.OriginalURL, nil
+	_, err = s.file.Write(data)
+	if err != nil {
+		return "", err
+	}
+	s.Links[urlID] = fullLink
+
+	// Add the URL to UserURLs
+	s.UserURLs[uid.(string)] = append(s.UserURLs[uid.(string)], models.UsersURLS{OriginalURL: fullLink, ShortURL: urlID})
+
+	return urlID, nil
 }
 
-func (m *MemoryStorage) ShortenURLBatch(ctx context.Context, batch []models.URLBatchRequest, baseAddr string) ([]models.URLRBatchResponse, error) {
+func (s *storage) GetFullURL(ctx context.Context, shortLink string) (string, error) {
+	val, ok := s.Links[shortLink]
+	if !ok {
+		return "", errors.New("link does not exist")
+	}
+	return val, nil
+}
+
+func (s *storage) ShortenURLBatch(ctx context.Context, batch []models.URLBatchRequest, baseAddr string) ([]models.URLRBatchResponse, error) {
 	if len(batch) == 0 {
 		return nil, errors.New("empty batch")
 	}
 
-	userID := ctx.Value("userID").(string)
 	responses := make([]models.URLRBatchResponse, len(batch))
+	uid := ctx.Value("userID")
 
 	for i, req := range batch {
-		shortURL := utils.Base62Encode(rand.Uint64())
-
-		if _, exists := m.urlMap[shortURL]; exists {
-			return nil, errors.New("short url already exists")
-		}
-
-		url := &models.URL{
-			ShortURL:    shortURL,
+		urlID := utils.Base62Encode(rand.Uint64())
+		urlInfo := URLInfo{
+			UUID:        uid.(string),
+			ShortURL:    urlID,
 			OriginalURL: req.OriginalURL,
-			UserID:      userID,
 		}
 
-		m.urlMap[shortURL] = url
-		m.userMap[userID] = append(m.userMap[userID], url)
+		data, err := json.Marshal(&urlInfo)
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = s.file.Write(data)
+		if err != nil {
+			return nil, err
+		}
+
+		s.Links[urlID] = req.OriginalURL
+		// Add the URL to UserURLs
+		s.UserURLs[uid.(string)] = append(s.UserURLs[uid.(string)], models.UsersURLS{OriginalURL: req.OriginalURL, ShortURL: urlID})
 
 		responses[i] = models.URLRBatchResponse{
 			CorrelationID: req.CorrelationID,
-			ShortURL:      baseAddr + "/" + shortURL,
+			ShortURL:      baseAddr + "/" + urlID,
 		}
 	}
 
