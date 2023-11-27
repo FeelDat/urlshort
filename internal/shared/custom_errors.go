24ed6ea2e88e536caf69301708a8228d698a6746
feeldat <alimaldybergenof@gmail.com>
2023-11-28 00:10:22 +0600

    runnin godoc test

--- internal/shared/custom_errors.go
+++ internal/shared/custom_errors.go
@@ -2,6 +2,8 @@ package shared
 
 import "github.com/pkg/errors"
 
+// ErrLinkDeleted is returned when a link is deleted.
+// ErrLinkNotExists is returned when a link does not exist.
 var (
 	ErrLinkDeleted   = errors.New("link is deleted")
 	ErrLinkNotExists = errors.New("link does not exist")
