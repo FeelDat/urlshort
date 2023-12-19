814dcea65a363d5574bfc6d2385a1eb8247af21b
feeldat <alimaldybergenof@gmail.com>
2023-12-20 00:55:38 +0600

    added godoc comments for subrouter

--- internal/app/routers/shortener_router.go
+++ internal/app/routers/shortener_router.go
@@ -7,8 +7,20 @@ import (
 	"net/http"
 )
 
+// ShortenerRouter creates and returns a new HTTP handler that implements
+// the routing logic for the URL shortener application. It defines routes
+// for URL shortening operations and associated API endpoints.
+// The router includes the following routes:
+// - POST /: Shortens a given URL.
+// - POST /api/shorten: Shortens a URL via JSON request.
+// - POST /api/shorten/batch: Shortens multiple URLs in a batch.
+// - GET /user/urls: Retrieves URLs associated with a user.
+// - DELETE /user/urls: Deletes URLs associated with a user.
+// - GET /{id}: Redirects to the full URL corresponding to the given ID.
+// - GET /swagger/*: Serves Swagger documentation.
+// The function requires a handlers.Handler struct that contains the necessary
+// handlers for the above routes.
 func ShortenerRouter(h handlers.Handler) http.Handler {
-
 	r := chi.NewRouter()
 
 	// Routing configuration
